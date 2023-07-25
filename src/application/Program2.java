a6f419a57fbbe66fc00ebd544fdaa600e4067729
fanecovsf <gustavo_h07@hotmail.com>
2023-07-25 14:36:25 -0300

    🆕 Feature:
    Finalizado o programa com DepartmentDao implementado

---
+++ src/application/Program2.java
@@ -0,0 +1,19 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		Department dep = new Department(3, "BI");
+		departmentDao.update(dep);
+		
+
+	}
+
+}
